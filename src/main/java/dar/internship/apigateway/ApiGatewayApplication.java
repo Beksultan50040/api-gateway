853bf2b1a8169c0efb65c38c9d829fdca0adfb28
Beksultan50040 <Beksultan.melisov@gmail.com>
2021-09-16 15:28:39 +0600

    Initial commit

---
+++ src/main/java/dar/internship/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package dar.internship.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dar/internship/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dar.internship.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
